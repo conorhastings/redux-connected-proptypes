46d2aae9d84d0f9766cc9f36afa5e4525118439d
conorhastings <hastings.conorm@gmail.com>
2015-12-14 20:39:04 -0500

    add jsdoc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import { connect } from 'react-redux';
+/**
+ * Takes in a React Component and returns connected Component with props defined on PropTypes
+ * injected into the Component
+ * 
+ * @param {React Component} component - the React Component to have props injected
+ * @param {array} [ignored] - optional paramter specifying keys defined on propTypes to not inject from
+ * global state
+ * @returns {React Component} returns React Component with props injected subscribed to change from 
+ * global redux state.
+ */
 export default function reduxConnectedPropTypes(component, ignored = []) {
   const mapProps = state => {
     let props = {};
